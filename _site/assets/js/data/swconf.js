240e211190c03a09e76f221d807f200ab42d24bb
KevAlx21 <101875990+KevAlx21@users.noreply.github.com>
2025-05-29 12:48:15 +0700

    Deleting link to my previous post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1748497188',resources: [
+    cacheName: 'chirpy-1748497667',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
