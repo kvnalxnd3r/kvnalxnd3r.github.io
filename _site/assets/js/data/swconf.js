cfb0040faf1987166f3e7e0ceeb3197b8e777f53
KevAlx21 <101875990+KevAlx21@users.noreply.github.com>
2025-05-29 12:49:56 +0700

    Adding again link to my previous post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1748497667',resources: [
+    cacheName: 'chirpy-1748497782',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
