880d73dbae40528427cad483d607dce53e438e07
KevAlx21 <101875990+KevAlx21@users.noreply.github.com>
2025-08-13 16:27:45 +0700

    Posting Bandit OverTheWire Level 9 and Level 10 challenge

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755004585',resources: [
+    cacheName: 'chirpy-1755077224',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
